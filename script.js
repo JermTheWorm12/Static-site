19de0ac21da2d9b3ade5f499d694540a2c68ba03
JermTheWorm12 <thepurpletophat13@gmail.com>
2026-04-09 09:19:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+fetch("data/files.json")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("files");
 
+    data.forEach(file => {
+      const div = document.createElement("div");
+      div.className = "file";
+
+      div.innerHTML = `
+        <h2>${file.title}</h2>
+        <p><strong>Class:</strong> ${file.class}</p>
+        <p>${file.description}</p>
+      `;
+
+      container.appendChild(div);
+    });
+  });
